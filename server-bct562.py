b5695a75efbb402ad64b267a59a3a8c363e7f126
toriobrandon <btorio@cs.utexas.edu>
2016-11-16 23:04:04 -0600

    lots of changes

--- server-bct562.py
+++ server-bct562.py
@@ -7,7 +7,8 @@ import time
 import os.path
 import sys
 import datetime
-import ipaddress 
+import ipaddress
+#import ipnetwork
 
 serverName = 'localhost'
 serverPort = int(sys.argv[1])
@@ -19,14 +20,14 @@ serverSocket.listen(1)
 #server can receive messages updating ruoting information 
 #server can receive questions asking where it would route a message
 #this is a dict of lists
-rtable = {'A' : [], 
-		  'B' : [], 
-		  'C' : [], 
-		  'D' : [],
-		  'E' : [], 
-		  'F' : [],
-		  'G' : [],
-		  'H' : []
+rtable = {'A' : ['0.0.0.0', 99], 
+		  'B' : ['0.0.0.0', 99], 
+		  'C' : ['0.0.0.0', 99], 
+		  'D' : ['0.0.0.0', 99],
+		  'E' : ['0.0.0.0', 99],
+		  'F' : ['0.0.0.0', 99],
+		  'G' : ['0.0.0.0', 99],
+		  'H' : ['0.0.0.0', 99]
 		  }
 
 #rtable = []
@@ -38,16 +39,17 @@ def parse_message(input):
 
 	if(words[0] == "UPDATE"):
 		print("it's an update")
-		perform_update(input)
+		perform_update(words)
 
 	if(words[0] == "QUERY"):
 		print("it's a query")
+		perform_query(words)
 		#answer the query
 	
 
 	return 
 
-def perform_update(input):
+def perform_update(words):
 	#example of update 
 	# UPDATE <cr><lf>
 	# A<sp>200.34.55.0/24<sp>22<cr><lf>
@@ -56,18 +58,38 @@ def perform_update(input):
 	# C<sp>200.34.0.0/16<sp>41<cr><lf>
 	# END<cr><lf>
 	
-	#iterate over the input
-	for word in input:
-		rtable[input[1]] = [input[1]]
-
+	#iterate over the input, update rtable accordingly 
+	i = 1
+	#print(len(words))
+	while i < (len(words) - 2):
+		rtable[words[i]] += [words[i+1], words[i + 2]]
+		#print(i)
+		i += 3
+	print (rtable)
 	response = "ACK\r\n"
 	response += "END\r\n"
-	
-	connectionSocket.send(response)
+	print(response)
+	connectionSocket.send(response.encode())
 
 	return 
 
-def receive_query():
+def perform_query(words):
+	ip = ipaddress.ip_address(words[1])
+	weight = 0
+
+	print(rtable)
+	for router in rtable:
+		i = 0
+		while i < len(router):
+			#if ip address is in this subnet
+			print(router[i])
+			ipNet = ipaddress.ip_network((rtable[router])[0])
+			if(ip in ipNet):
+				print("the ip address is in subnet" + ipNet)
+				if(rtable[router[i + 1]] < weight):
+					weight = rtable[router[i + 1]]
+			i += 2
+
 
 	return
 
@@ -75,6 +97,7 @@ def receive_query():
 while 1:
 	connectionSocket, addr = serverSocket.accept()
 	sentence = connectionSocket.recv(2048)
+	sentence = str(sentence.decode())
 	parse_message(sentence)
 	
 	#print(sentence)
